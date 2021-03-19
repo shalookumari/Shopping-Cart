164398e745b7766acf78c0825011988ac6d19aa9
P99003615 <akashsingh@ltts.com>
2021-03-19 17:34:15 +0530

    Signin & Registration

---
+++ Team_2_ShoppingCart/src/main/java/com/ltts/shopping/web/MainController.java
@@ -0,0 +1,20 @@
+
+package com.ltts.shopping.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
