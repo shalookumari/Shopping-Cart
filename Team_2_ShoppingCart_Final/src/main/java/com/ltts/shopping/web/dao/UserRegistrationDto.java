0bde17bc95ce10e673235132ff5e23db58f44f33
shalookumari <78845430+shalookumari@users.noreply.github.com>
2021-03-30 15:31:15 +0530

    Update UserRegistrationDto.java

--- Team_2_ShoppingCart_Final/src/main/java/com/ltts/shopping/web/dao/UserRegistrationDto.java
+++ Team_2_ShoppingCart_Final/src/main/java/com/ltts/shopping/web/dao/UserRegistrationDto.java
@@ -1,23 +1,46 @@
 package com.ltts.shopping.web.dao;
 
+import java.util.Date;
+
 public class UserRegistrationDto {
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
+	private String contact;
+	private String date;
 	
 	public UserRegistrationDto(){
 		
 	}
 	
-	public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+	public UserRegistrationDto(String firstName, String lastName, String email, String password, String contact,
+			String date) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
+		this.contact = contact;
+		this.date = date;
 	}
-	
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -42,4 +65,12 @@ public class UserRegistrationDto {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	@Override
+	public String toString() {
+		return "UserRegistrationDto [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", contact=" + contact + ", date=" + date + "]";
+	}
+	
+	
 }
